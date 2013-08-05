ec6c46fddf78f086ed5b0f924d24f2eb108ae55d
Alex Rothenberg <alex@alexrothenberg.com>
2013-08-05 14:01:37 -0400

    Initial commit: yo angular tic-tac-toe

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ticTacToeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
